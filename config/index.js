b738bef91351943f4757211dae50643169d14da6
Sergii Ivanchenko <ivanchenko.s@gmail.com>
2017-08-17 15:13:02 +0300

    Display. Add mail send. Configure GCP SQL. test v0.6

--- config/index.js
+++ config/index.js
@@ -1,19 +1,19 @@
 module.exports = {
-    db_params:{
-    host:'localhost',
-    port:'3306',
-    user:'root',
-    password:'123456',
-    database:'data',
-    socketPath:'/cloudsql/circular-source-174612:europe-west1:display-db'
+    db_params: {
+        host: '35.187.178.187',
+        port: '3306',
+        user: 'root',
+        password: '123456',
+        database: 'data',
+        socketPath: '/cloudsql/circular-source-174612:europe-west1:display-db'
     },
-    port:process.env.port || 8081,
-    google_oAuthClient:{
-        clientID:'874875351866-og6jt96hbse1slh9fhm7a0fc3hcb24bb.apps.googleusercontent.com',
-        clientSecret:'bd8rayeX0S2gSN__Ms_5YEcU',
-        refreshToken:'1/T6sv6mUCg9NCn8Z4SPMhAeCfzjyyG2wL623GjKiIF9U'
+    port: process.env.port || 8081,
+    google_oAuthClient: {
+        clientID: '874875351866-og6jt96hbse1slh9fhm7a0fc3hcb24bb.apps.googleusercontent.com',
+        clientSecret: 'bd8rayeX0S2gSN__Ms_5YEcU',
+        refreshToken: '1/T6sv6mUCg9NCn8Z4SPMhAeCfzjyyG2wL623GjKiIF9U'
     },
-    adminEmail:'ivanchenko.s@gmail.com'
+    adminEmail: 'ivanchenko.s@gmail.com'
 };
 
 
